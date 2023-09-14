d063a5f4f52cf95d4a0b16f06c7705c61ef66e45
Francois Lambert <francoisobob@gmail.com>
2023-09-14 09:25:19 -0400

    Initial Commit

---
+++ SkillBuilding/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  SkillBuilding
+//
+//  Created by Francois Lambert on 9/14/23.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
